5a2a2e129dedf667241df57b99767550bbddf886
hiagomenezes <hiago@itecnologia.com.br>
2019-08-02 16:06:45 -0300

    atualizou

---
+++ calculadora/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculadora calc = new Calculadora();
+
+            Console.Write("Digite o valor do raio: ");
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double circ = calc.Circunferencia(raio);
+            double vol = calc.Volume(raio);
+            
+            Console.WriteLine("Circunferencia: " + circ.ToString("n2", CultureInfo.InvariantCulture));
+            Console.WriteLine("volume: " + vol.ToString("n2", CultureInfo.InvariantCulture));
+            Console.WriteLine("valor de PI: " + calc.Pi.ToString("n2", CultureInfo.InvariantCulture));
+        }
+    }
+}
